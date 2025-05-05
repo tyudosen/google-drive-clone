08f2ae2b6446c9d1238d58f92b68e4e755cdcd22
toyo <toyo@Toyos-MBP.lan>
2025-05-05 14:12:05 -0500

    initial setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLE_STORE_HOST,
+    user: env.SINGLE_STORE_USER,
+    password: env.SINGLE_STORE_PASSWORD,
+    port: env.SINGLE_STORE_PORT as never as number,
+    database: env.SINGLE_STORE_DATABASE_NAME,
+    ssl: {},
   },
   tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
--- src/env.js
+++ src/env.js
@@ -7,10 +7,14 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    SINGLE_STORE_USER: z.string(),
+    SINGLE_STORE_PASSWORD: z.string(),
+    SINGLE_STORE_HOST: z.string(),
+    SINGLE_STORE_PORT: z.string(),
+    SINGLE_STORE_DATABASE_NAME: z.string(),
   },
 
   /**
@@ -27,9 +31,12 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    SINGLE_STORE_USER: process.env.SINGLE_STORE_USER,
+    SINGLE_STORE_PASSWORD: process.env.SINGLE_STORE_PASSWORD,
+    SINGLE_STORE_HOST: process.env.SINGLE_STORE_HOST,
+    SINGLE_STORE_PORT: process.env.SINGLE_STORE_PORT,
+    SINGLE_STORE_DATABASE_NAME: process.env.SINGLE_STORE_DATABASE_NAME,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
